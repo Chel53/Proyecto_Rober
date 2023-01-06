ec827c3942df6b33f6336c1910cb3490dc7d039d
JacielH <111033129+JacielH@users.noreply.github.com>
2023-01-06 15:26:17 -0600

    MainActivity Listo

--- ExamenRob3/app/src/main/java/com/example/examenrob3/MainActivity.java
+++ ExamenRob3/app/src/main/java/com/example/examenrob3/MainActivity.java
@@ -2,13 +2,52 @@ package com.example.examenrob3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnMenuOptC1Huella, btnMenuOptC2Temperatura, btnMenuOptC3LucesOff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnMenuOptC1Huella = (Button) findViewById( R.id.btnMenuOptC1Huella );
+        btnMenuOptC2Temperatura = (Button) findViewById( R.id.btnMenuOptC2Temperatura );
+        btnMenuOptC3LucesOff = (Button) findViewById( R.id.btnMenuOptC3LucesOff );
+
+        btnMenuOptC1Huella.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //TODO: Redirigir a la Pantalla (Acticity) :: Circuito 1: Huella
+                Intent i = new Intent(MainActivity.this, //ControlPuerta.class);
+                startActivity(i);
+            }
+        }); //--fin: btnMenuOptC1Huella.clickListener()
+
+        btnMenuOptC2Temperatura.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                //TODO: Redirigir a la Pantalla (Acticity) :: Circuito 2: Temperatura Corporal
+                // -->> Enviar a pantalla de C2TemperaturaActivity
+                Intent intent = new Intent(MainActivity.this, //C2TemperaturaActivity.class);
+                startActivity( intent );
+                finish();
+            }
+        }); //--fin: btnMenuOptC2Temperatura.clickListener()
+
+        btnMenuOptC3LucesOff.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                //TODO: Redirigir a la Pantalla (Acticity) :: Circuito 3: Luces de Oficina
+                Intent i = new Intent(MainActivity.this, //ControlLuz.class);
+                startActivity(i);
+            }
+        }); //--fin: btnMenuOptC3LucesOff.clickListener()
     }
 }
