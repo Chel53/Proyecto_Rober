773302f9411911e010ac04e6b590937575b457e8
Angel Alonso Rodriguez <55955398+xElxAGUADORX@users.noreply.github.com>
2023-01-07 16:06:35 -0600

    ITZEL hizo CAMBIOS en el DISEÑO del MENÚ

--- ExamenRob3/app/src/main/java/com/example/examenrob3/MainActivity.java
+++ ExamenRob3/app/src/main/java/com/example/examenrob3/MainActivity.java
@@ -6,18 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnMenuOptC1Huella, btnMenuOptC2Temperatura, btnMenuOptC3LucesOff;
+    ImageButton btnMenuOptC1Huella, btnMenuOptC2Temperatura, btnMenuOptC3LucesOff;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnMenuOptC1Huella = (Button) findViewById( R.id.btnMenuOptC1Huella );
-        btnMenuOptC2Temperatura = (Button) findViewById( R.id.btnMenuOptC2Temperatura );
-        btnMenuOptC3LucesOff = (Button) findViewById( R.id.btnMenuOptC3LucesOff );
+        btnMenuOptC1Huella = (ImageButton) findViewById( R.id.btnMenuOptC1Huella );
+        btnMenuOptC2Temperatura = (ImageButton) findViewById( R.id.btnMenuOptC2Temperatura );
+        btnMenuOptC3LucesOff = (ImageButton) findViewById( R.id.btnMenuOptC3LucesOff );
 
         btnMenuOptC1Huella.setOnClickListener(new View.OnClickListener(){
             @Override
