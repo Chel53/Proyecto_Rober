ad3d24e05deda0069fd50e8ba83f3df0fc7f228e
Angel Alonso Rodriguez <55955398+xElxAGUADORX@users.noreply.github.com>
2023-01-07 18:08:03 -0600

    ITZEL cambio DISEÑO en Act.LUZ

--- ExamenRob3/app/src/main/java/com/example/examenrob3/Luz.java
+++ ExamenRob3/app/src/main/java/com/example/examenrob3/Luz.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.Toast;
 
@@ -27,41 +28,10 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class Luz extends AppCompatActivity {
-    // Consumo CONTINUO de WS consultarEstatusLucesOficina [000webhost-eec]
-    private class ConsultarEstatusLucesOficinaTask extends TimerTask {
-
-        @Override
-        public void run() {
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    //TODO: Implementar llamada a WS consultarTempCorp() y actualizar la Temperatura en el
-                    // textView [tvValorTemperaturaCorp]
-                    // Toast.makeText( getApplicationContext(), "Consultando temperatura V2...", Toast.LENGTH_SHORT).show();
-                    imgBtnEstatusLuz = (ImageButton) findViewById( R.id.imgBtnEstatusLuz );
-
-                    /*
-                    String urlTmp = "http://" + WSConfig.serverIP + "/ws_punto_venta_android/validarLoginUsuario.php?usuario=" + cTUsuario.getText().toString() + "&password=" + cTContrasenia.getText().toString();
-
-                    validarUsuario( urlTmp );
-                    */
-
-                    // tvValorTemperaturaCorp.setText( "66.6 °Cel" );
-                    // String urlConsultaTempCorp = "http://localhost/ws_punto_venta_android/random_temperatures.php";
-                    // http://192.168.0.108/ws_punto_venta_android/random_temperatures.php
-
-                    consultarEstatusLucesOficina( urlConsultarEstatusLucesOficina );
-
-                    // getSupportFragmentManager().findFragmentById()
-                } //--fin: run()
-            });
-        } //--fin: run()
-
-    } //--fin :: Private Class :: ConsultarTemperaturaTask
 
     Switch switchLucesOficina;
     Integer  statusControlLuz;
-    ImageButton imgBtnEstatusLuz;
+    ImageView imgBtnEstatusLuz,imgBtnEstatusLuz1,imgBtnEstatusLuz2;
     Button btnBackToHomeL;
     ImageButton btnReloadLuz;
 
@@ -82,7 +52,9 @@ public class Luz extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_luz);
         switchLucesOficina = (Switch) findViewById(R.id.switchLucesOficina);
-        imgBtnEstatusLuz = (ImageButton) findViewById(R.id.imgBtnEstatusLuz);
+        imgBtnEstatusLuz = (ImageView) findViewById(R.id.imgBtnEstatusLuz);
+        imgBtnEstatusLuz1 = (ImageView) findViewById(R.id.imgBtnEstatusLuz1);
+        imgBtnEstatusLuz2 = (ImageView) findViewById(R.id.imgBtnEstatusLuz2);
 
         imgBtnEstatusLuz.setBackgroundDrawable(null);
 
@@ -93,16 +65,15 @@ public class Luz extends AppCompatActivity {
             -- INIT :: Apagado
         */
         statusControlLuz = 0; // Apagadas
-        switchLucesOficina.setChecked( false ); // Apagadas
-        switchLucesOficina.setText( "Encender" ); // Apagadas
-        imgBtnEstatusLuz.setImageResource(R.drawable.apagado); // Apagadas
+        //switchLucesOficina.setChecked( false ); // Apagadas
+       //switchLucesOficina.setText( "Encender" ); // Apagadas
+        //imgBtnEstatusLuz.setImageResource(R.drawable.lamparapag); // Apagadas
 
 
 
         btnBackToHomeL = (Button) findViewById( R.id.btnBackToHomeL );
-        //btnReloadTLuz = (ImageButton) findViewById( R.id.btnReloadLuz);
 
-        // validarstatusLuz();
+        validarstatusLuz();
 
 
         switchLucesOficina.setOnClickListener(new View.OnClickListener() {
@@ -115,23 +86,22 @@ public class Luz extends AppCompatActivity {
                     // se desea APAGAR las luces...
                     // INTENCIÓN de APAGAR las luces
                     Toast.makeText( getApplicationContext(), "Apagando las luces...", Toast.LENGTH_SHORT).show();
-                    controlApagarLuces();
+                    //controlApagarLuces();
                     // DESPUÉS de haber APAGADO las LUCES... [Toogle :: ${statusControlLuz = 0} :: 0 :: Apagado]
-                    statusControlLuz = 0; // Apagadas
-                    switchLucesOficina.setChecked( false ); // Apagadas
-                    switchLucesOficina.setText( "ENCENDER" );
-                    // validarstatusLuz();
+                   //statusControlLuz = 0; // Apagadas
+                    //switchLucesOficina.setChecked( false ); // Apagadas
+                   // switchLucesOficina.setText( "ENCENDER" );
+                    validarstatusLuz();
                 } else {
                     // Luces APAGADAS Actualmente [statusControlLuz == 0]
                     // se desea ENCENDER las luces...
                     // INTENCIÓN de ENCENDER las luces
                     Toast.makeText( getApplicationContext(), "Encendiendo las luces...", Toast.LENGTH_SHORT).show();
-                    controlEncenderLuces();
+                    //controlEncenderLuces();
                     // DESPUÉS de haber ENCENDIDO las LUCES... [Toogle :: ${statusControlLuz = 1} :: 1 ::  ENCENDIDO]
-                    statusControlLuz = 1; // ENCENDIDO
-                    switchLucesOficina.setChecked( true );
-                    switchLucesOficina.setText( "Apagar" );
-                    // validarstatusLuz();
+                    //statusControlLuz = 1; // ENCENDIDO
+                    ////switchLucesOficina.setText( "Apagar" );
+                    validarstatusLuz();
                 }
 
             }
@@ -160,17 +130,17 @@ public class Luz extends AppCompatActivity {
         // TODO: inicializar [statusLucesOff] de ACUERDO al valor recuperado del WS
         // consumo constante cada 350 ms
         timer = new Timer();
-        timer.scheduleAtFixedRate( new ConsultarEstatusLucesOficinaTask(), 0, 350 );
+        //timer.scheduleAtFixedRate( new ConsultarEstatusLucesOficinaTask(), 0, 350 );
 
     } //--fin: onCreate()
 
-    private void controlApagarLuces(){
-        String url = "https://conceptos-web-2010067-eec.000webhostapp.com/control_leds.php?num_led=2" + "&valor=0";
+    private void controlApagarLuces(int numfoco){
+        String url = "https://conceptos-web-2010067-eec.000webhostapp.com/control_leds.php?num_led="+ numfoco+ "&valor=0";
         actualizarLEDEnBD( url );
     } //--fin: controlApagarLuces()
 
-    private void controlEncenderLuces(){
-        String url = "https://conceptos-web-2010067-eec.000webhostapp.com/control_leds.php?num_led=2" + "&valor=1";
+    private void controlEncenderLuces(int numfoco){
+        String url = "https://conceptos-web-2010067-eec.000webhostapp.com/control_leds.php?num_led="+ numfoco+ "&valor=1";
         actualizarLEDEnBD( url );
     } //--fin: controlEncenderLuces()
 
@@ -197,136 +167,32 @@ public class Luz extends AppCompatActivity {
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add( stringRequest );
     }
-    //++FIN: actualizarLEDEnBD(url : String)
 
-    /*
-    statusLuz == 1 ::  ENCENDIDO
-    statusLuz == 0 :: Apagado
-    -- INIT :: Apagado
-    */
 
-    //--INI: consultarEstatusLucesOficina( url: String )
-    private void consultarEstatusLucesOficina(String url){
 
-        // String nombreCompletoTmp;
-        // Toast.makeText( getApplicationContext(), url, Toast.LENGTH_LONG ).show();
-
-        // Para JsonArrayRequest :: default :: método GET de Http
-
-        JsonArrayRequest stringArray = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
-
-            @Override
-            public void onResponse(JSONArray response) {
-                JSONObject jsonObject = null;
-
-                // Toast.makeText( getApplicationContext(), response.toString(), Toast.LENGTH_LONG ).show();
-                // String nombreEmpleado = "";
-                // String strTemperatura = "", strTimestamp = "";
-                int accessCode, estatusLuces;
-
-                for (int i = 0; i < response.length(); i++) {
-
-                    try {
-                        jsonObject = response.getJSONObject(i);
-                        // ctNombre.setText(jsonObject.getString("nombre"));
-                        // ctEstado.setText( jsonObject.getString("estado") );
-                        // Integer.toString(123)
-                        // Toast.makeText(getApplicationContext(), Integer.toString( jsonObject.getInt("code") ) , Toast.LENGTH_LONG ).show();
-                        accessCode = jsonObject.getInt( "code" );
-                        /*
-                         */
-                        estatusLuces = jsonObject.getInt("estatus_luces");
-
-                        // Toast.makeText( getApplicationContext(), nombreEmpleado, Toast.LENGTH_LONG ).show();
-
-                        if( accessCode == 100 ){
-                            // OK : Dejar pasar al Usuario...
-                            // Registrar nombreCompleto en archivo Preferences..,
-                            // registrarNombreEmpleado( nombreEmpleado );
-                            // registrarIDEmpleado( jsonObject.getInt( "id_empleado" ) );
-                            // -->> Enviar a pantalla de MENU Principal...
-                            /*
-                            Intent intent = new Intent(LoginActivity.this, MenuPrincipalActivity.class);
-                            startActivity( intent );
-                            finish();
-                            */
-                              /*
-                                statusPuer = 1;
-                                1 :: 'Abierta'
-                                0 :: 'Cerrada'
-                            */
-                            // tvValorTemperaturaCorp.setText( strTemperatura + " °C" );
-
-                            // MOD. dinámica de IMG...
-                            if( estatusLuces == 1 ){
-                                imgBtnEstatusLuz.setImageResource( R.drawable.encendido );
-                            }else{
-                                // Apagadas ::
-                                imgBtnEstatusLuz.setImageResource( R.drawable.apagado );
-                            }
-
-                            // tVValorTimestamp.setText( strTimestamp );
-                        }else{
-                            Toast.makeText( getApplicationContext(), "ERROR al recurepar Estatus Luces de Off. del WS", Toast.LENGTH_LONG).show();
-                            // labelMsgLogin.setText( "Usuario y contraseña NO coinciden." );
-                        } //-- fin : IF accessCode == 100 (OK - Dejar pasar)
-
-                    } catch (JSONException e) {
-                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
-                    }catch(Exception e){
-                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
-                    }
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
-            }
-        }){
-
-        };
-
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add( stringArray );
-
-        // return accessCode;
-    }
-    //++FIN: consultarEstatusLucesOficina( url: String )
-
-    /*
     public void validarstatusLuz(){
-        if (statusControlLuz==1){//Cuando el switch es checado
+        if (statusControlLuz==1){
+            statusControlLuz = 0;
             switchLucesOficina.setChecked(true);
             switchLucesOficina.setText("Apagar la luz");
-            imgBtnEstatusLuz.setImageResource(R.drawable.encendido);
+            imgBtnEstatusLuz.setImageResource(R.drawable.lamparapren);
+            imgBtnEstatusLuz1.setImageResource(R.drawable.lamparapren);
+            imgBtnEstatusLuz2.setImageResource(R.drawable.lamparapren);
             Toast.makeText(this, "ON", Toast.LENGTH_SHORT).show();
 
         }else{
+            statusControlLuz = 1;
             switchLucesOficina.setChecked(false);
             switchLucesOficina.setText("Encender la luz");
-            imgBtnEstatusLuz.setImageResource(R.drawable.apagado);
+            imgBtnEstatusLuz.setImageResource(R.drawable.lamparapag);
+            imgBtnEstatusLuz1.setImageResource(R.drawable.lamparapag);
+            imgBtnEstatusLuz2.setImageResource(R.drawable.lamparapag);
             Toast.makeText(this, "OFF", Toast.LENGTH_SHORT).show();
 
 
         }
     } //-- fin: validarstatusLuz()
-    */
 
-    private void actualizar(String url) {
-        StringRequest stringRequest = new StringRequest(Request.Method.PUT, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                Toast.makeText(getApplicationContext(), "operción realiizada", Toast.LENGTH_LONG).show();
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_LONG).show();
-            }
-        });
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(stringRequest);
 
-    }
+
 }
