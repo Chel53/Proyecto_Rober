2d7e90cc1e17cc36419d3edfb120f028cb30df00
erescamillac <erescamillac@gmail.com>
2023-01-07 19:17:33 -0600

    Consumo de WS consulta lumenes
    Consumo de WS consulta de lumenes

--- ExamenRob3/app/src/main/java/com/example/examenrob3/Luz.java
+++ ExamenRob3/app/src/main/java/com/example/examenrob3/Luz.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -29,13 +30,47 @@ import java.util.TimerTask;
 
 public class Luz extends AppCompatActivity {
 
+    // Consumo CONTINUO de WS consultarLumenes [000webhost-robert]
+    private class ConsultarLumenesTask extends TimerTask {
+
+        @Override
+        public void run() {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    //TODO: Implementar llamada a WS consultarLumenes() y actualizar el valor de [LUMENES] en el
+                    // textView [tvValorLumenes]
+                    // Toast.makeText( getApplicationContext(), "Consultando LUMENES V2...", Toast.LENGTH_SHORT).show();
+                    tvValorLumenes = findViewById( R.id.tvValorLumenes );
+
+                    /*
+                    String urlTmp = "http://" + WSConfig.serverIP + "/ws_punto_venta_android/validarLoginUsuario.php?usuario=" + cTUsuario.getText().toString() + "&password=" + cTContrasenia.getText().toString();
+
+                    validarUsuario( urlTmp );
+                    */
+
+                    // tvValorTemperaturaCorp.setText( "66.6 °Cel" );
+                    // String urlConsultaTempCorp = "http://localhost/ws_punto_venta_android/random_temperatures.php";
+                    // http://192.168.0.108/ws_punto_venta_android/random_temperatures.php
+
+                    consultarLumenesBD( urlConsultarLumenes );
+
+                    // getSupportFragmentManager().findFragmentById()
+                } //--fin: run()
+            });
+        } //--fin: run()
+
+    } //--fin :: Private Class :: ConsultarTemperaturaTask
+
     Switch switchLucesOficina;
     Integer  statusControlLuz;
     ImageView imgBtnEstatusLuz,imgBtnEstatusLuz1,imgBtnEstatusLuz2;
     Button btnBackToHomeL;
     ImageButton btnReloadLuz;
 
-    String urlConsultarEstatusLucesOficina = "https://conceptos-web-2010067-eec.000webhostapp.com/consultar_estatus_luces.php";
+    TextView tvValorLumenes;
+
+    String urlConsultarLumenes = "https://pruebasoooo.000webhostapp.com/consultar_lumenes.php";
 
     /*
     statusControlLuz == 1 ::  ENCENDIDO
@@ -55,6 +90,8 @@ public class Luz extends AppCompatActivity {
         imgBtnEstatusLuz = (ImageView) findViewById(R.id.imgBtnEstatusLuz);
         imgBtnEstatusLuz1 = (ImageView) findViewById(R.id.imgBtnEstatusLuz1);
         imgBtnEstatusLuz2 = (ImageView) findViewById(R.id.imgBtnEstatusLuz2);
+
+        tvValorLumenes = (TextView) findViewById( R.id.tvValorLumenes );
 
         imgBtnEstatusLuz.setBackgroundDrawable(null);
 
@@ -131,8 +168,92 @@ public class Luz extends AppCompatActivity {
         // consumo constante cada 350 ms
         timer = new Timer();
         //timer.scheduleAtFixedRate( new ConsultarEstatusLucesOficinaTask(), 0, 350 );
+        timer.scheduleAtFixedRate( new ConsultarLumenesTask(), 0, 350 );
 
     } //--fin: onCreate()
+
+    //--INI: consultarLumenesBD( url: String )
+    private void consultarLumenesBD(String url){
+
+        // String nombreCompletoTmp;
+        // Toast.makeText( getApplicationContext(), url, Toast.LENGTH_LONG ).show();
+
+        // Para JsonArrayRequest :: default :: método GET de Http
+
+        JsonArrayRequest stringArray = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
+
+            @Override
+            public void onResponse(JSONArray response) {
+                JSONObject jsonObject = null;
+
+                // Toast.makeText( getApplicationContext(), response.toString(), Toast.LENGTH_LONG ).show();
+                // String nombreEmpleado = ""; lux
+                String strLumenes = "";
+
+                int accessCode;
+
+                for (int i = 0; i < response.length(); i++) {
+
+                    try {
+                        jsonObject = response.getJSONObject(i);
+                        // ctNombre.setText(jsonObject.getString("nombre"));
+                        // ctEstado.setText( jsonObject.getString("estado") );
+                        // Integer.toString(123)
+                        // Toast.makeText(getApplicationContext(), Integer.toString( jsonObject.getInt("code") ) , Toast.LENGTH_LONG ).show();
+                        accessCode = jsonObject.getInt( "code" );
+                        /*
+                        nombreEmpleado += jsonObject.getString( "nombre" );
+                        nombreEmpleado += " ";
+                        nombreEmpleado += jsonObject.getString( "ap_pat" );
+                        nombreEmpleado += " ";
+                        nombreEmpleado += jsonObject.getString( "ap_mat" );
+                        labelMsgLogin.setText( Integer.toString(accessCode) );
+                        */
+                        strLumenes = jsonObject.getString("lumenes" );
+                        // strTimestamp = jsonObject.getString( "timestamp_temperatura" );
+
+                        // Toast.makeText( getApplicationContext(), nombreEmpleado, Toast.LENGTH_LONG ).show();
+
+                        if( accessCode == 100 ){
+                            // OK : Dejar pasar al Usuario...
+                            // Registrar nombreCompleto en archivo Preferences..,
+                            // registrarNombreEmpleado( nombreEmpleado );
+                            // registrarIDEmpleado( jsonObject.getInt( "id_empleado" ) );
+                            // -->> Enviar a pantalla de MENU Principal...
+                            /*
+                            Intent intent = new Intent(LoginActivity.this, MenuPrincipalActivity.class);
+                            startActivity( intent );
+                            finish();
+                            */
+                            tvValorLumenes.setText( strLumenes + " lux" );
+                            // tVValorTimestamp.setText( strTimestamp );
+                        }else{
+                            Toast.makeText( getApplicationContext(), "ERROR al recurepar Temp. Corp. del WS", Toast.LENGTH_LONG).show();
+                            // labelMsgLogin.setText( "Usuario y contraseña NO coinciden." );
+                        } //-- fin : IF accessCode == 100 (OK - Dejar pasar)
+
+                    } catch (JSONException e) {
+                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                    }catch(Exception e){
+                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }){
+
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add( stringArray );
+
+        // return accessCode;
+    }
+    //++FIN: consultarLumenesBD( url: String )
 
     private void controlApagarLuces(int numfoco){
         String url = "https://conceptos-web-2010067-eec.000webhostapp.com/control_leds.php?num_led="+ numfoco+ "&valor=0";
