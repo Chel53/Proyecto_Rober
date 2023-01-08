18cc16b57bb5e58f7f2f4a002ccce28b39e778e9
Angel Alonso Rodriguez <55955398+xElxAGUADORX@users.noreply.github.com>
2023-01-07 18:45:33 -0600

    Cambio LOGO y Creación de ACT.VENTILADOR (Itzel)

---
+++ ExamenRob3/app/src/main/java/com/example/examenrob3/Ventilador.java
@@ -0,0 +1,14 @@
+package com.example.examenrob3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ventilador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ventilador);
+    }
+}
